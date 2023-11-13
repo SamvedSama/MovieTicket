4c9b98f9df8bacb72536ff233580d9c18f7fe0b3
Sairam S <saisr2206@gmail.com>
2023-11-13 08:11:47 +0530

    .....

--- MovieTicketReservation.py
+++ MovieTicketReservation.py
@@ -4,7 +4,6 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["User_Details"]
 collection = mydb["user"]
-x = mydb.collection.find({},{'_id':0})
 dict = {}
 def welcome():
     print("Welcome to Movie Ticket Reservation System")
@@ -16,15 +15,16 @@ def create_user():
     print("User Successfully Created")
 
 def login_user():
+    x = mydb.collection.find({},{'_id':0})
     user = input("Please enter your name: ")
     password = input("Please enter your password here: ")
     for y in x:
         for z in y:
-            
-            if user==z and password==y[z]:
-                print(f"Logged in as {user} successfully! ")
-            elif(user != z and password != y[z]):
-                print("Wrong Username or Password detected.")
+            dict[z]=y[z]
+    if user in dict and dict[user]==password:
+        print(f"Login as {user} Successfull")
+    else :
+        print("Check your Credentials") 
 def display_movies():
     movie_list = ["Kantaara","KGF","Modiji","Motte","ChakDeIndia"]
     print("The movie options are: ")
