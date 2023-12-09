e1a3b3b49c44e0a75df8ba7c05aa385e2e3b8401
Sairam S <125436937+S-Sairam@users.noreply.github.com>
2023-12-09 22:54:41 +0530

    Update proj.py
    some GUI needed

--- proj.py
+++ proj.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import sys,requi,reserve,login
+import sys,requi,reserve,login,ticket,admin,datetime,random,string
 from datetime import date,timedelta
 
 
@@ -69,13 +69,22 @@ def finish_create():
     global b
     global c
     global d
-    global e
+    global e,M_Reserved
     a=user.get()                    #Username
     b=password.get()                #Pass1
     c=password2.get()               #Pass2
     d=mail.get()                    #Email1
     e=mail2.get()                   #Email2
     if b==c and d.endswith("@gmail.com") and d==e:
+        #updates new user in db
+        date = requi.dat
+        M_Reserved={}
+        c2 = requi.encrypt(password)
+        dic = {date:M_Reserved}
+        requi.mydb.collection.insert_one({'name':a,'password':c2,'email':e})
+        requi.collection.update_one({"name":a},{'$set':{"movie_reserved": dic }})
+        requi.collection.update_one({"name":a},{'$set':{"book_count": 0 }})   
+        #updated db    
         notif1.config(fg="green",text="User created Successfully")
         Create_screen.after(1000,Create_screen.destroy)
     else:
@@ -93,9 +102,9 @@ def Login():
     l2.grid(row=3,sticky=W)
     l3=Label(Login_screen,text="Enter Password:",font=('Calibri',12))
     l3.grid(row=4,sticky=W) 
-    global usr
-    global fmail
-    global pwd
+    global usr # usr username
+    global fmail #fmail email
+    global pwd 
     usr=StringVar()
     fmail=StringVar()
     pwd=StringVar()
@@ -112,11 +121,14 @@ def Login():
     b1.grid(row=5)
 
 def finish_login():
-    global a1
+    global a1,a3
     a1=usr.get()
     a2=pwd.get()
     a3=fmail.get()
-    if a1==a and a2==b and a3==d:
+    x = requi.mydb.collection.find_one({'name':a1},{"_id":0})
+    b = x["password"]
+    d = x["email"]
+    if a1==[x["name"]] and a2==b and a3==d:
         notif2.config(fg="green",text="Login Successful")
         Login_screen.destroy()
         global acct_dashboard
@@ -124,97 +136,32 @@ def finish_login():
         acct_dashboard.title("Account dashboard")
         Label(acct_dashboard,text="Select your choice from the given below:",font=('Calibri',12)).grid(row=0,sticky=N)
         Button(acct_dashboard,text="Book a ticket",command=book,padx=20).grid(row=2)
-        Button(acct_dashboard,text="View booked tickets",padx=20).grid(row=3)
+        Button(acct_dashboard,text="View booked tickets",padx=20).grid(row=3)#make viewbooked
         Button(acct_dashboard,text="Logout",command=Logout,padx=20).grid(row=4)
+    elif a1=="Admin" and a2=='':
+        admin.admin()
     else:
         notif2.config(fg="red",text="Invalid username or password! Try again")
-
-def Kantara():
-    global Kantara_screen
-    Kantara_screen=Toplevel(root)
-    Kantara_screen.title("Kantara")
-    l=Label(Kantara_screen,text="Select number of tickets:",font=('Çalibri',12))
-    l.grid(row=0,sticky=N)
-    n=IntVar()
-    n.set(1) 
-    global tickets   
-    tickets= OptionMenu(Kantara_screen,n,1,2,3,4,5,6,7,8).grid(row=1) #Creates a menu box
-    l=Label(Kantara_screen,text=n.get(),font=('Çalibri',12))
-    l.grid(row=2,sticky=N)
-
-def KGF_2():
-    global KGF_2_screen
-    KGF_2_screen=Toplevel(root)
-    KGF_2_screen.title("KGF 2")
-    l=Label(KGF_2_screen,text="Select number of tickets:",font=('Çalibri',12))
-    l.grid(row=0,sticky=N)
-    n=IntVar()
-    n.set(1)  
-    global tickets  
-    tickets= OptionMenu(KGF_2_screen,n,1,2,3,4,5,6,7,8) #Creates a menu box
-    l=Label(KGF_2_screen,text=n.get(),font=('Çalibri',12)).grid(row=1)
-    l.grid(row=2,sticky=N)
-
-def Jailer():
-    global Jailer_screen
-    Jailer_screen=Toplevel(root)
-    Jailer_screen.title("Jailer")
-    l=Label(Jailer_screen,text="Select number of tickets:",font=('Çalibri',12))
-    l.grid(row=0,sticky=N)
-    n=IntVar()
-    n.set(1) 
-    global tickets   
-    tickets= OptionMenu(Jailer_screen,n,1,2,3,4,5,6,7,8).grid(row=1) #Creates a menu box
-    l=Label(Jailer_screen,text=n.get(),font=('Çalibri',12))
-    l.grid(row=2,sticky=N)
-
-def Three_Idiots():
-    global Three_Idiots_screen
-    Three_Idiots_screen=Toplevel(root)
-    Three_Idiots_screen.title("3 Idiots")
-    l=Label(Three_Idiots_screen,text="Select number of tickets:",font=('Çalibri',12))
-    l.grid(row=13,sticky=N)
-    n=IntVar()
-    n.set(1)  
-    global tickets  
-    tickets= OptionMenu(Three_Idiots_screen,n,1,2,3,4,5,6,7,8).grid(row=1) #Creates a menu box
-    l=Label(Three_Idiots_screen,text=n.get(),font=('Çalibri',12))
-    l.grid(row=2,sticky=N)
-
-def Avengers_Endgame():
-    global Avengers_Endgame_screen
-    Avengers_Endgame_screen=Toplevel(root)
-    Avengers_Endgame_screen.title("Avengers Endgame")
-    l=Label(Avengers_Endgame_screen,text="Select number of tickets:",font=('Çalibri',12))
-    l.grid(row=0,sticky=N)
-    n=IntVar()
-    n.set(1)   
-    global tickets 
-    tickets= OptionMenu(Avengers_Endgame_screen,n,1,2,3,4,5,6,7,8).grid(row=1) #Creates a menu box
-    l=Label(Avengers_Endgame_screen,text=n.get(),font=('Çalibri',12))
-    l.grid(row=2,sticky=N)
 
 def Display():
     global Display_screen
     requi.os.system("cls")
-    global movie_list
+    global movie_options
     Display_screen=Toplevel(root)
     Display_screen.title("Display movies")
     l=Label(Display_screen,text="The Movies available are:",font=('Çalibri',12))
     l.grid(row=0,sticky=N)
-    l=Label(Display_screen,text="Kantara",font=('Çalibri',12))
-    l.grid(row=1,sticky=W)
-    l=Label(Display_screen,text="KGF 2",font=('Çalibri',12))
-    l.grid(row=2,sticky=W)
-    l=Label(Display_screen,text="Jailer",font=('Çalibri',12))
-    l.grid(row=3,sticky=W)
-    l=Label(Display_screen,text="3 Idiots",font=('Çalibri',12))
-    l.grid(row=4,sticky=W)
-    l=Label(Display_screen,text="Avengers:Endgame",font=('Çalibri',12))
-    l.grid(row=5,sticky=W)
+    movie_options=[]
+    buffer_movie=requi.movies.find({},{"_id":0,"seats_available":0})                    #Accessing db and showing moves in db for dropdown
+    for i in buffer_movie:
+        for key in i:
+            movie_options.append(i[key])
+    for i in range(len(movie_options)):
+        l=Label(Display_screen,text=movie_options[i],font=('Çalibri',12))
+        l.grid(row=i+1,sticky=W)
 
 def proceed_to_seat_selection(movie_name,num_tickets):
-    global Proceed_screen,m_name
+    global Proceed_screen,m_name,selected_seats
     Proceed_screen=Toplevel(root)
     Proceed_screen.title("Seat Selection")
     Label(Proceed_screen,text="Select your seats:",font=('Calibri', 12)).grid(row=0)
@@ -223,10 +170,11 @@ def proceed_to_seat_selection(movie_name,num_tickets):
     row_index=1
     col_index=0
     buttons=[]  # List to store references to the seat buttons
-    selected_seats=set()  # Set to store selected seat numbers
+    selected_seats=[]  # Set to store selected seat numbers
 
     def check(y):
         global m_name,book_date
+        global selected_seats
         x = requi.movies.find_one({'Name':m_name})
         seats_available= x["seats_available"]
         try : 
@@ -249,18 +197,22 @@ def proceed_to_seat_selection(movie_name,num_tickets):
     seats = list(map(check,list(i for i in range(1,65))))
 
     def toggle_seat(seat_number):
-        nonlocal selected_seats
+        global selected_seats
         if seat_number in selected_seats:
             buttons[seat_number-1].config(state='normal',bg='SystemButtonFace',command=lambda num=seat_number: toggle_seat(num))
             selected_seats.remove(seat_number)
         else:
             buttons[seat_number-1].config(state='disabled',bg='gray',command=lambda: None)
-            selected_seats.add(seat_number)
+            selected_seats.append(seat_number)
 
     
     for i in range(len(seats)):
-        seat_button=Button(Proceed_screen,text=f"{seats[i]}",width=5,
-                             command=lambda num=i+1: toggle_seat(num))
+        if seats[i] == 'RS': #doesnt allow for rebook
+            seat_button=Button(Proceed_screen,text=f"{seats[i]}",width=5,
+                                command=lambda num=i+1: toggle_seat(num),state = 'disabled')
+        else :
+                seat_button=Button(Proceed_screen,text=f"{seats[i]}",width=5,
+                                command=lambda num=i+1: toggle_seat(num),state = 'normal')    
         seat_button.grid(row=row_index,column=col_index)
         buttons.append(seat_button)  # Store the button reference in the list
         col_index+=1
@@ -269,21 +221,23 @@ def proceed_to_seat_selection(movie_name,num_tickets):
             col_index=0
 
     def pay_now():
+        global num_seats
         if len(selected_seats)!=num_tickets:
-            Label(Proceed_screen,text="Please select {} seats.".format(num_tickets),font=('Calibri', 12),
+            Label(Proceed_screen,text=f"Please select {num_tickets} seats.",font=('Calibri', 12),
                   fg="red").grid(row=12)
         else:
             # Mock implementation of payment processing
-            payment_gateway=PaymentGateway()
-            if payment_gateway.process_payment(num_tickets*500):
-                Label(Proceed_screen,text="Payment successful!,{} has been paid".format(num_tickets*500),font=('Calibri', 12),fg="green").grid(row=12)
+            # payment_gateway=PaymentGateway()
+            payment_gateway = pay(num_seats)
+            if payment_gateway["transaction_status"]==True:
+                update_db()
                 # Add code here to store the booking details or update a database
             else:
                 Label(Proceed_screen,text="Payment failed.There was an error! Please try again", font=('Calibri', 12),
                       fg="red").grid(row=12)
 
     def cancel_selection():
-        nonlocal selected_seats   # used for referring to a variable in the nearest outer scope
+        global selected_seats   # used for referring to a variable in the nearest outer scope
         for seat_number in selected_seats:
             buttons[seat_number-1].config(state='normal',bg='SystemButtonFace',command=lambda num=seat_number: toggle_seat(num))
         selected_seats=set()
@@ -294,8 +248,8 @@ def proceed_to_seat_selection(movie_name,num_tickets):
 
 
 def book():
-    global Book_screen,book_date
-    global m_name
+    global Book_screen,book_date,num_seats
+    global m_name,movie_options
     movie_options = []
     Book_screen = Toplevel(root)
     Book_screen.title("Bookings")
@@ -313,9 +267,11 @@ def book():
     Label(Book_screen, text="Select number of tickets:", font=('Calibri', 12)).grid(row=2)
     num_tickets_var = IntVar()
     num_tickets_var.set(1)
+    
     num_tickets_options = [i for i in range(1, 9)]  # 1 to 8 tickets
     num_tickets_dropdown = OptionMenu(Book_screen, num_tickets_var, *num_tickets_options)
     num_tickets_dropdown.grid(row=2, column=1)
+    num_seats = num_tickets_var.get()
     Label(Book_screen, text="Enter the date you want to watch it on:", font=('Calibri', 12)).grid(row=3)
     date_var = StringVar()
     tomorrow= date.today()+timedelta(days=1)
@@ -330,7 +286,7 @@ def book():
         l1 += i + '/'
     l1 = l1[0:-1]
     book_date = l1
-
+    
 
 
     # Button to proceed to seat selection
@@ -346,6 +302,81 @@ def Logout():
 def Exit():
     print("Successfully logged out")
     sys.exit()
+def update_db():
+        global a1,book_date,m_name,selected_seats
+        num_seats = selected_seats
+        uname=a1
+        dat=book_date
+        M_name = m_name
+        y = requi.collection.find_one({"name":uname})
+        d = y["movie_reserved"]
+        try :
+            movie_seats=d[dat]
+        except KeyError :
+            d[dat]={M_name:[]}
+            movie_seats=d[dat]
+        seat_numbers=list()
+        try :
+            seat_numbers=movie_seats[M_name]
+        except KeyError:
+            movie_seats[M_name]=[]    
+        for i in num_seats:
+                seat_numbers.append(i)
+        movie_seats[M_name]=seat_numbers
+        d[dat] = movie_seats
+        requi.collection.update_one({"name":(uname)},{'$set':{"movie_reserved":d}})  
+        ticket.ticket_gen()
+        print("Seats Reserved")
+        a = requi.movies.find_one({"Name":M_name})
+        b = a["seats_available"]
+        try :
+            c = b[login.date]
+        except KeyError:
+            b[login.date]=[]
+            c = b[login.date]
+        c.append(num_seats)
+        b[login.date]=c
+        y = requi.movies.find_one({'Name':M_name})                                          
+        requi.movies.update_one({'Name':M_name},{'$set':{'seats_available' : b}})
+        requi.collection.update_one({"name":uname},{'$inc':{"book_count": 1 }})
+def pay(seat_num):
+    ticket_price = 500
+    global total_cost,status
+    total_cost = seat_num * ticket_price
+    x = requi.collection.find_one({'name':login.uname})
+    try:
+        if x["book_count"]>=5 and x["book_count"]<15:
+            total_cost = 0.95*total_cost
+            Label(Proceed_screen,text="5% Discount applied ",font=('Calibri', 12),fg="green").grid(row=12)             
+        elif x["book_count"]>=15:
+            total_cost = 0.8*total_cost
+            Label(Proceed_screen,text="20% Discount applied ",font=('Calibri', 12),fg="green").grid(row=12) 
+    except KeyError:
+        pass
+    
+    #INSERT RADIO BUTTON BS
+
+    Label(Proceed_screen,text=f"Amount Payable is {total_cost} ",font=('Calibri', 12),fg="green").grid(row=12) 
+    
+    
+    print("Proceed with payment ?(Y/N/Yes/No) ")
+    choice=input()
+    if choice.lower() in "yyes":
+        status=True
+    else:
+        status=False
+    return generate_random_transaction()
+
+
+
+
+def generate_random_transaction():
+    global status
+    transaction_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    amount = total_cost
+    transaction_data = {"transaction_id": transaction_id,"amount": amount,"transaction_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"transaction_status":status}
+    return transaction_data
+
 
 myButton1=Button(root,text="Create user",padx=20,command=Create)
 myButton1.pack()
