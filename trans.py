801be02e4897442f82555edab6ae25d8a8c7a54e
SamvedSama <106664875+SamvedSama@users.noreply.github.com>
2023-12-09 11:10:23 +0530

    Update trans.py

--- trans.py
+++ trans.py
@@ -1,19 +1,24 @@
 import requi,login
-total_cost = 0
 import random
 import string
 from datetime import datetime
+
+total_cost = 0
+
 def pay(seat_num):
     ticket_price = 500
     global total_cost,status
     total_cost = seat_num * ticket_price
     x = requi.collection.find_one({'name':login.uname})
-    if x["book_count"]>=5 and x["book_count"]<15:
-        total_cost = 0.95*total_cost
-        print("Discount of 5% applied")
-    elif x["book_count"]>=15:
-        total_cost = 0.8*total_cost
-        print("Discount of 20% applied")
+    try:
+        if x["book_count"]>=5 and x["book_count"]<15:
+            total_cost = 0.95*total_cost
+            print("Discount of 5% applied")
+        elif x["book_count"]>=15:
+            total_cost = 0.8*total_cost
+            print("Discount of 20% applied")
+    except KeyError:
+        pass
     print(f"Amount payable for {seat_num} is {total_cost}")
     print("Proceed with payment ?(Y/N/Yes/No) ")
     choice=input()
@@ -22,6 +27,7 @@ def pay(seat_num):
     else:
         status=False
     return generate_random_transaction()
+
 def generate_random_transaction():
     global status
     transaction_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
